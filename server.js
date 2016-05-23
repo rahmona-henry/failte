ec473ff965c9d850da43a9d8fa2d59396aa182c0
Rahmona Henry <email": null>
2016-05-23 20:18:37 +1200

    Add cha functionality

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+
+var users =[]
+var connections =[]
+
+server.listen(process.env.port || 3000)
+console.log('Server running')
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+})
+//Create Connection
+io.sockets.on('connection', function(socket){
+    connections.push(socket)
+    console.log('Connected %s sockets connected', connections.length)
+})
+//Disconnect Connection
+io.sockets.on('disconnect', function(data){
+    users.splice(users.indexOf(socket.username),1)
+    updateUsernames()
+    connections.splice(connections.indexOf(socket),1)
+    console.log('Disconnected: %s sockets connected', connections.length)
+      })
+
+//Send Message
+io.sockets.on('send message', function(data){
+    console.log(data)
+    io.sockets.emit('new message',{msg:data})
+  })
+
+
+//New users
+io.sockets.on('new user', function(data, callback){
+    callback(true)
+    socket.username = data
+    users.push(socket.username)
+    updateUsernames()
+
+})
+
+function updateUsernames(){
+    io.sockets.emit('get users', users)
+}
